19f9e6d132ffae54a6dc2e85b41058b8a819cfa8
Aidan Doherty <119699042+aidandoherty01@users.noreply.github.com>
2025-02-13 20:47:38 -0500

    Implemented database functionality in getData.cjs
    getData reads from the Mongodb Database and writes to ./data/exported_data.json

---
+++ Coding-Crazy/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Game from "./Game";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Game time</h1>
+        <Game />
+      </div>
+    </>
+  );
+}
+
+export default App;
