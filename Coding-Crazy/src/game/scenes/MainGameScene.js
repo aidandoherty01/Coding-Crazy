1d82ad72ef59db89da393eee0e98cc8e50f51f62
BuildTools <unconfigured@null.spigotmc.org>
2025-03-01 17:50:49 -0500

    remove stuff

--- Coding-Crazy/src/game/scenes/MainGameScene.js
+++ Coding-Crazy/src/game/scenes/MainGameScene.js
@@ -15,20 +15,6 @@ class MainGameScene extends Phaser.Scene {
     console.log("🎮 MainGameScene is now active!");
     this.add.image(0, 0, "board").setOrigin(0).setScale(0.5);
 
-    // Add player animation frames
-    const playerFrames = [
-      { x: 0, y: 0, frame: 0 },
-      { x: 0, y: 100, frame: 1 },
-      { x: 0, y: 200, frame: 2 },
-      { x: 0, y: 300, frame: 3 },
-      { x: 100, y: 0, frame: 4 },
-      { x: 200, y: 0, frame: 5 },
-      { x: 300, y: 0, frame: 6 },
-    ];
-
-    playerFrames.forEach(({ x, y, frame }) => {
-      this.add.image(x, y, "player", frame).setOrigin(0);
-    });
     this.scene.launch("BoardScene");
     new StartQuestionButton(this);
     // Emit an event to notify the React component that the scene is ready
